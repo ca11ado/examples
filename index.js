5be7f7497653980262d49d1063281b024a43b7d5
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-21 16:05:03 +0300

    promises

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+'use strict';
 require('babel-register');
+
+let myPromise = new Promise((resolve, reject) => {
+  let myObj = {
+    one: 'one',
+    two: 'two'
+  };
+  resolve(myObj);
+});
+
+
+myPromise
+  .then((res) => {
+    console.log(res);
+    return res;
+  })
+  .then(res => {
+    console.log(res);
+  });
