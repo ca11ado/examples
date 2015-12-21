224981a5d34cbd48b2eacca4caace9ea994132bf
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-21 17:35:34 +0300

    uses _.partial to add arguments

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 require('babel-register');
+let _ = require('lodash');
 
 let myPromise = new Promise((resolve, reject) => {
   let myObj = {
@@ -12,9 +13,41 @@ let myPromise = new Promise((resolve, reject) => {
 
 myPromise
   .then((res) => {
-    console.log(res);
+    //console.log(res);
     return res;
   })
   .then(res => {
+    //console.log(res);
+  });
+
+
+let partObj = {
+  ficus: {
+    code: 200
+  }
+};
+
+
+let mySecondPromise = new Promise((resolve, reject) => {
+  let myObj = {
+    one: 'one',
+    two: 'two',
+    code: 200
+  };
+  resolve(myObj);
+});
+
+mySecondPromise
+  .then(_.partialRight(checkStatus, partObj))
+  .then(res => {
     console.log(res);
   });
+
+
+function checkStatus(data) {
+  console.log(arguments);
+  if (_.get(data, ['status', 'code']) == 200) {
+    data.status.code = 400;
+  }
+  return data;
+}
