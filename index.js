35db8908a6c6bc738a0ccd17fc743740bb98ce41
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-29 16:49:01 +0300

    classes

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 'use strict';
 require('babel-register');
 
+class MyNewClass {
+  constructor (name) {
+    this.name = name;
+  }
+
+  showName () {
+    console.log(this.name);
+  }
+
+  showGreetName () {
+    console.log(this.greet(this.name));
+  }
+
+  /*static greet (text) {
+    return `Greet ${text}`;
+  }*/
+
+  greet (text) {
+    return `Greet ${text}`;
+  }
+}
+
+
+let myName = new MyNewClass('t0s');
+
+myName.showName();
+myName.showGreetName();
+
+class MyNextClass extends MyNewClass {
+  showAge () {
+    super.showName();
+  }
+}
+
+let myNextClass = new MyNextClass(13);
+myNextClass.showAge();
