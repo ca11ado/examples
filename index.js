40c8a31fb36f0a21a44628ef65c576330bee3a0c
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2015-12-30 11:25:07 +0300

    ZipCode function

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 'use strict';
 require('babel-register');
 
+function ZipCode(code, location) {
+  var _code = code;
+  var _location = location || '';
+
+  return {
+    code: function () {
+      return _code;
+    },
+    location: function () {
+      return _location;
+    },
+    fromString: function (str) {
+      var parts = str.split('-');
+      return ZipCode(parts[0], parts[1]);
+    },
+    toString: function () {
+      return _code + '-' + _location;
+    }
+  };
+}
+
+const prinsetonZip = ZipCode('08544', 3345);
+console.log(prinsetonZip.toString());
+prinsetonZip.code();
+console.log(prinsetonZip.toString());
