cd5d5dc1677105aeb30c230255e532d5bd4f8a70
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-01-01 20:05:44 +0300

    New commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict';
 require('babel-register');
 
+function negate(func) { //#A
+  return function() {
+    //return !func.apply(null, arguments); //#B
+    //console.log(arguments);
+    return !func(arguments[0]); //#B
+  };
+}
+function isNull(val) { //#C
+  console.log(val);
+  return val === null;
+}
+var isNotNull = negate(isNull); //#D
+isNotNull(null); //-> false
+isNotNull({});   //-> true»
+
+console.log(isNotNull(null));
+console.log(isNotNull({}));
