e1f57f9f42019ea6d2e6dd27f9778d63620f46e5
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-01-01 19:02:28 +0300

    tfdf

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 require('babel-register');
+require('babel-polyfill');
 
+//checkDestr({one: 'one', two: 'two', second: 'second'});
+let { one, two } = {one: 'one', two: 'two', second: 'second'};
+console.log(one, two);
+
+function checkDestr ({ one, two }) {
+  console.log(one, two);
+}
