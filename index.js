ef494350afd3cb9b0052c2ed708ad786426b3758
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-03-21 13:59:21 +0300

    step number unkonwn

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 'use strict';
 require('babel-register');
 
+var _ = require('lodash');
+var moment = require('moment');
+
+var format = 'DD.MM.YYYY';
+var date4 = '02.04.2015';
+var SPLIT_DATE = '01.07.2015';
+
+
+/*console.log('---');
+console.log(moment(date4, format) > moment(SPLIT_DATE, format));
+console.log(moment(date4, format) < moment(SPLIT_DATE, format));*/
+
+for (let i=1; i < 31; i++) {
+  console.log(moment(`${i}.07.2015`, format) < moment(SPLIT_DATE, format));
+} 
