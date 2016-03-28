3c0df06a600e6c07bb93b3129248f8b18364df74
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-03-28 22:07:17 +0300

    simple main functions

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 'use strict';
 require('babel-register');
 
+var R = require('ramda');
+
+var someArray = [1, 2, 3, 4, 5, 6];
+
+// MAP
+
+var double = (a) => a*a;
+var testMap = R.map(double, someArray);
+console.log(testMap);
+
+var doubleAccum = (a, b) => [a*b, a*b];
+var testAccumMap = R.mapAccum(doubleAccum, 1, someArray);
+console.log(testAccumMap);
+
+// REDUCE
+
+var summReduce = (a, b) => a + b;
+var testReduce = R.reduce(summReduce, 0, someArray);
+console.log(testReduce);
