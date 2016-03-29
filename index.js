4cbe92c554b0488b4f9cc923ee3759d38c1d2606
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-03-29 11:37:02 +0300

    some another examples

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 require('babel-register');
 require('babel-polyfill');
 
+// !!!need to use flag --harmony_destructuring with node!!!
+
 //checkDestr({one: 'one', two: 'two', second: 'second'});
 let { one, two } = {one: 'one', two: 'two', second: 'second'};
 console.log(one, two);
@@ -9,3 +11,13 @@ console.log(one, two);
 function checkDestr ({ one, two }) {
   console.log(one, two);
 }
+
+var { checked: test } = { checked: true, someElse: false };
+
+console.log(test);
+
+function destr ({ checked: status }) {
+  return status;
+}
+
+console.log(destr({ checked: true }));
