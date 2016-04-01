67ca08a3dc14961b85afe67ae58d958fbaa3d3ca
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-04-01 18:30:20 +0300

    reduce

--- index.js
+++ index.js
@@ -18,51 +18,42 @@ let testCollection2 = [
   { id: 5, address: 'Moscow', isNew: true, checkedAt: null, response: { code: 200 } }
 ];
 
-let testObject = {
-  testCollection
-};
+let intersectionCollection = [
+  {
+    status: {
+      code: 200
+    },
+    content: {
+      result: {
+        items: [
+          { id: 1, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } },
+          { id: 2, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } }
+        ]
+      }
+    }
+  },
 
-let result = _.chain(_.get(testObject, 'testCollection'))
-  .filter({ isNew: false })
-  .filter((item) => _.isFinite(_.get(item, 'checkedAt')))
-  .size()
+  {
+    status: {
+      code: 200
+    },
+    content: {
+      result: {
+        items: [
+          { id: 1, address: 'Piter', isNew: false, checkedAt: 1234, response: { code: 200 } },
+          { id: 2, address: 'Piter', isNew: true, checkedAt: 1234, response: { code: 200 } }
+        ]
+      }
+    }
+  }
+];
+
+console.log(` reduce ------------`);
+let testIntersection = _.chain(intersectionCollection)
+  .reduce((result, value, key) => {
+    var items = _.get(value, 'content.result.items');
+    return [ ...result, ...items];
+  }, [])
+  .find({ isNew: true })
   .value();
-
-console.log(result);
-
-console.log(_.every(testCollection, { address: 'Moscow' }));
-
-console.log(' ----------- ');
-let pickTest = _.chain(testCollection)
-  .filter({ id: 10 })
-  .head()
-  .pick('address')
-  .value();
-
-console.log(pickTest);
-
-console.log(` reject test ------------`);
-
-let rejectTest = _.chain(testCollection)
-  .reject({ id: 2 })
-  .value();
-console.log(rejectTest);
-
-console.log(` find test ------------`);
-let findTest = _.chain(testCollection)
-  .find({ id: 2 })
-  .value();
-console.log(findTest);
-
-console.log(` math ceil ------------`);
-console.log(_.ceil(43.02));
-
-console.log(` creating array ------------`);
-var myArray = _.chain(new Array(_.ceil(99 / 10)))
-  .map((val, index) => 'test' + index)
-  .value();
-console.log(myArray);
-
-console.log(` every ------------`);
-let testEvery = _.every(testCollection2, { response: { code: 200 } });
-console.log(testEvery);
+console.log(testIntersection);
