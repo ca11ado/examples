abd62162c52b1efdbbefe5b2faf8784fd71b1d3d
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-05-20 20:08:34 +0300

    example with {{{ }}}

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ let Handlebars = require('handlebars');
 
 var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
   "{{kids.length}} kids:</p>" +
-  "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+  "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>" +
+  "{{{widthHtml}}}";
 
 var template = Handlebars.compile(source);
 
 var data = {
-  name: "Alan", 
+  name: "Alan",
+  widthHtml: "<div>with HTML</div>",
   hometown: "Somewhere, TX",
   kids: [{ name: "Jimmy", age: "12" }, { name: "Sally", age: "4" }]
 };
