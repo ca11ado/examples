e72c43591ef5fae92745489afe1b53f10b5d737f
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-05-20 20:57:14 +0300

    example with registerHelper that works with invoked parameters

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 'use strict';
 require('babel-register');
+let _ = require('lodash');
 let Handlebars = require('handlebars');
+
+
+Handlebars.registerHelper('rz', function (context, options) {
+  console.log('Handlebars registerHelper');
+  console.log(context);
+  //console.log(options);
+  //console.log(this);
+  let result = _.reduce(context, (summ, letter) => {
+    return summ + letter + ' ';
+  }, '');
+  return result;
+});
 
 var source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
   "{{kids.length}} kids:</p>" +
   "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>" +
-  "{{{widthHtml}}}";
+  "{{{widthHtml}}}" +
+  "{{#rz name}}{{/rz}}";
 
 var template = Handlebars.compile(source);
-
 var data = {
   name: "Alan",
   widthHtml: "<div>with HTML</div>",
@@ -17,4 +30,5 @@ var data = {
 };
 var result = template(data);
 
+console.log('---- result');
 console.log(result);
