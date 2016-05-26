ad25d583259fa942170ac648a8352959f8fb9d13
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-05-26 10:07:47 +0300

    merge

--- index.js
+++ index.js
@@ -4,79 +4,20 @@ var _ = require('lodash');
 
 // --harmony_destructuring
 
-let testCollection = [
-  { id: 1, address: 'Moscow', isNew: false, checkedAt: 1234 },
-  { id: 2, address: 'Moscow', isNew: false, checkedAt: 1234 },
-  { id: 3, address: 'Moscow', isNew: false, checkedAt: 1234 },
-  { id: 4, address: 'Moscow', isNew: false, checkedAt: false },
-  { id: 5, address: 'Moscow', isNew: true, checkedAt: null }
+let firstCollection = [
+  { city: 'Moscow', items: [{ address: 'Lenina 1' }, { address: 'Lenina 2' }] },
+  { city: 'Piter', items: [{ address: 'Marks 1' }, { address: 'Marks 2' }] }
 ];
 
-let testCollection2 = [
-  { id: 1, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } },
-  { id: 2, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } },
-  { id: 3, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } },
-  { id: 4, address: 'Moscow', isNew: false, checkedAt: false, response: { code: 200 } },
-  { id: 5, address: 'Moscow', isNew: true, checkedAt: null, response: { code: 200 } }
+let secondCollection = [
+  { city: 'Moscow', items: [{ address: 'Lenina 3' }, { address: 'Lenina 4' }] },
+  { city: 'Piter', items: [{ address: 'Marks 3' }, { address: 'Marks 4' }] }
+];
+let thirdCollection = [
+  { city: 'Moscow', items: [{ address: 'Lenina 5' }, { address: 'Lenina 6' }] },
+  { city: 'Piter', items: [{ address: 'Marks 5' }, { address: 'Marks 6' }] },
+  { city: 'Himki', items: [{ address: 'Stalina 1' }, { address: 'Stalina 2' }] }
 ];
 
-let testCollection3 = {
-  headBranch: false,
-  address: undefined,
-  name: undefined,
-  streen: undefined
-};
-
-let intersectionCollection = [
-  {
-    status: {
-      code: 200
-    },
-    content: {
-      result: {
-        items: [
-          { id: 1, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } },
-          { id: 2, address: 'Moscow', isNew: false, checkedAt: 1234, response: { code: 200 } }
-        ]
-      }
-    }
-  },
-
-  {
-    status: {
-      code: 200
-    },
-    content: {
-      result: {
-        items: [
-          { id: 1, address: 'Piter', isNew: false, checkedAt: 1234, response: { code: 200 } },
-          { id: 2, address: 'Piter', isNew: true, checkedAt: 1234, response: { code: 200 } }
-        ]
-      }
-    }
-  }
-];
-
-console.log(` -------- let `);
-
-let { headBranch, streen } = testCollection3;
-console.log(headBranch, streen);
-
-console.log(` -------- перебор `);
-let number = 100;
-let sNumber = number.toString();
-let rNumber = _.reduceRight(sNumber, (prev, val, index) => {
-  //console.log(prev, val, index);
-  if ((index) % 3 === 0) {
-    prev = ' ' + prev;
-  }
-  return val + prev;
-}, '');
-console.log(rNumber);
-
-console.log(' --------- thousand separator');
-function numberWithCommas(x) {
-  /*return */x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-}
-let nn = 100000;
-console.log(numberWithCommas(nn));
+let merge = _.merge(firstCollection, secondCollection);
+console.log(merge);
