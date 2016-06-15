db651ca43e44dbbb316c15be0757b4e750efe988
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-06-15 15:52:48 +0300

    someshit

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 require('babel-register');
 var _ = require('lodash');
+
+let http = 'httpx:////////////2gis.ru';
+let http2 = 'http://localhost';
+let http3 = 'http://';
+
+const REG = /^(.+\/)?(.+\..+)$/;
+const PROTOCOL = /.+?:\/\//;
+const BODY = /^(?!\/+?|\.+?|:+?).+\..+$/;
+
+let result = http3.match(REG);
+
+console.log(result);
+
+if (result) {
+  let [, protocol = '', body = ''] = result;
+
+  console.log(protocol, '    ', body);
+  console.log(PROTOCOL.test(protocol));
+  console.log(BODY.test(body));  
+}
