560c200a1dce3a6ca15911674aeb7552157ff7d9
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-06-16 09:03:18 +0300

    someshit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict';
 require('babel-register');
 
+let data = {
+  license: true
+};
+
+let str = [];
+for (let p in data) {
+  str.push(encodeURIComponent(p) + '=' + encodeURIComponent(data[p]));
+}
+str = str.join('&');
+
+let encodedData = encodeURIComponent(data);
+let encoded2Data = encodeURI(data);
+
+let emails = `["${data.emails.join('", "')}"]`;
+
+console.log(emails);
