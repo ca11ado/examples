490874cff1fe3b39e51fac09b9dec07f61bce460
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-06-21 10:05:34 +0300

    наследование

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
 'use strict';
 require('babel-register');
+let _ = require('lodash');
 
-class MyNewClass {
-  constructor (name) {
-    this.name = name;
+class Tree {
+  constructor (state) {
+    this._state = state;
   }
 
-  showName () {
-    return this.name;
-  }
-
-  showGreetName () {
-    console.log(this.greet(this.name));
-  }
-
-  /*static greet (text) {
-    return `Greet ${text}`;
-  }*/
-
-  greet (text) {
-    return `Greet ${text}`;
-  }
-
-  get PATHS () {
-    return {
-      path1: '1',
-      path2: '2'
-    }
+  _set (key, value) {
+    let path = this.constructor.path(key);
+    this._state.set(path, value);
   }
 }
 
+class Addresses extends Tree {
+  constructor (state, monkey, limit) {
+    super(state);
+    this._monkey = monkey;
+    this._limit = limit;
+  }
 
-let myName = new MyNewClass('t0s');
-
-myName.showName();
-myName.showGreetName();
-
-class MyNextClass extends MyNewClass {
+  /**
+   * Путь к инстансу в локаторе
+   * @returns {string}
+   */
+  static get className () {
+    return 'jungle.profile.isp.coverage.IspAddressesPageTree';
+  }
 }
 
-let myNextClass = new MyNextClass();
+class IspValidPageTree extends Addresses {
+  className () {
+    return super.constructor.className;
+  }
+  
+  getMonkey () {
+    return this._monkey;
+  }
+}
 
-/*console.log('------------ test getter');
-console.log(MyNewClass.PATHS);
-console.log(myName.PATHS);*/
+let isp = new IspValidPageTree('state', 'monkey', '1');
 
-console.log('------------ show now');
-console.log(myNextClass.showName());
+console.log(isp);
+console.log(isp.className());
+console.log(isp.getMonkey());
