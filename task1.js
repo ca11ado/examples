87dc91381171b6aec0312f77a57d9db8a89d99ba
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2016-12-05 13:35:22 +0300

    Мультивставка: insertAdjacentHTML и DocumentFragment

--- task1.js
+++
@@ -1,8 +0,0 @@
-let ul = document.createElement('ul');
-let userPrompt;
-while (userPrompt = prompt()) {
-  let li = document.createElement('li');
-  li.textContent = userPrompt;
-  ul.insertBefore(li, null);
-}
-document.body.insertBefore(ul, null);
