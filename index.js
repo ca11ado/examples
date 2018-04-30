7fd09cb3e8b3a0a3baa264e9950d856e841cd9c8
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2018-04-30 22:35:23 +0300

    add ramda

--- index.js
+++ index.js
@@ -6,22 +6,34 @@ import flow from 'lodash/fp/flow';
 import take from 'lodash/fp/take';
 import map from 'lodash/fp/map';
 import filter from 'lodash/fp/filter';
+import * as R from 'ramda';
 
 const add = v => {
   console.log('add func');
   return v + v;
+};
+const v2 = v => {
+  console.log('v2 func');
+  return v * v;
 };
 const onlyOdd = v => {
   console.log('odd filter');
   return v % 2 === 0;
 };
 
-const arr = times(200);
+/*const arr = times(200);
 
 const result = flow(
   map(add),
   filter(onlyOdd),
   take(3),
-)(arr);
+)(arr);*/
 
-console.log(result);
+const arrR = R.range(1, 200);
+const resultR = R.compose(
+  R.map(add),
+  R.filter(onlyOdd),
+  R.take(3)
+)(arrR);
+
+console.log(resultR);
