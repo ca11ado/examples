6f6ca4ae74897bf4ccaa31821e9bbe7f2994b208
Химцов Антон Игоревич <a.khimtsov@2gis.ru>
2018-04-30 21:42:16 +0300

    draft

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 'use strict';
 require('babel-register');
 
+import times from 'lodash/times';
+import flow from 'lodash/fp/flow';
+import take from 'lodash/fp/take';
+import map from 'lodash/fp/map';
+import filter from 'lodash/fp/filter';
+
+const add = v => {
+  console.log('add func');
+  return v + v;
+};
+const onlyOdd = v => {
+  console.log('odd filter');
+  return v % 2 === 0;
+};
+
+const arr = times(200);
+
+const result = flow(
+  map(add),
+  filter(onlyOdd),
+  take(3),
+)(arr);
+
+console.log(result);
