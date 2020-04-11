393e7bdcc9563fb771fd20bbecbec75c14e7dbb2
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2020-04-11 17:25:48 +0300

    first and second chapter of professor frisby

---
+++ helpers.js
@@ -0,0 +1,17 @@
+import curry from 'lodash/curry';
+
+export const summ = curry((b, a) => a + b);
+export const multiply = curry((b, a) => a * b);
+export const divide = curry((b, a) => a / b);
+
+export const getDigitsFromString = str => {
+  const result  = str.match(/(\d+)/);
+  return result && result.length && result[0];
+};
+
+export const addTax = curry((factor, price) => {
+  const tax = multiply(factor, price);
+  return summ(tax, price);
+});
+
+export const addCurrency = curry((currency, price) => `${price} ${currency}`);
--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-import module from './module.js';
+import * as helpers from './helpers';
 
-console.log('this is index.js file');
-module();
+const inspect = Symbol.for('nodejs.util.inspect.custom');
+
+function chapter(name) {
+  console.clear();
+  console.log(`CHAPTER: ${name}`);
+}
+
+function log(...texts) {
+  console.log('LOG: ', ...texts);
+}
+
+chapter('One. Container data type (Box)');
+const c1r1 = [0]
+  .map(helpers.summ(1))
+  .map(helpers.multiply(2))
+  [0];
+
+const Box = (x) => ({
+  map: func => Box(func(x)),
+  fold: func => func(x),
+  [inspect]: () => `Box(${x})`,
+});
+
+const c1r2 = Box(0)
+  .map(helpers.summ(1))
+  .map(helpers.multiply(2))
+
+log(c1r1);
+log(c1r2);
+
+chapter('Two. Single composed expression using Box');
+const firstProduct = 'The first price will be 1200 rub';
+const secondProduct = 'The second price will be 303 rub';
+
+const firstProductPriceWithTax = Box(firstProduct)
+  .map(helpers.getDigitsFromString)
+  .map(Number)
+  .fold(helpers.addTax(0.20));
+
+const secondProductPriceWithTax = Box(secondProduct)
+  .map(helpers.getDigitsFromString)
+  .map(Number)
+  .fold(helpers.addTax(0.20));
+
+const c2r1 = Box(secondProductPriceWithTax)
+  .map(helpers.summ(firstProductPriceWithTax))
+  .fold(helpers.addCurrency('rub'));
+
+log(c2r1);
+
+const getDigitsFromString = str => Box(helpers.getDigitsFromString(str));
+const addCurrency = (currency, price) => Box(helpers.addCurrency(currency, price));
+const addTax = (factor, price) => Box(helpers.addTax(factor, price));
+const summ = (a, b) => Box(helpers.summ(a, b));
+
+const c2r2 = getDigitsFromString(firstProduct)
+  .fold(price => 
+    addTax(0.13, price)
+      .fold(price => addCurrency('rub', price)))
+
+log(c2r2);
