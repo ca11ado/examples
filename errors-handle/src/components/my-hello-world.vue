eca741a5d509bdb3b4cf808adbcd508822539b16
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2020-04-19 21:02:34 +0300

    app backbone

---
+++ errors-handle/src/components/my-hello-world.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    My hello world
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {};
+  },
+  computed: {
+  },
+};
+</script>
+
+<style module>
+  .root {
+    display: block;
+  }
+</style>
